28811b5bd4a95dbd53e6cff4ce06b03a78a1aa7e
masozow <marvingjo@hotmail.co>
2015-10-02 10:21:17 -0600

    Agregar mas de un post a la plantilla

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Postear
 # Create your views here.
 def listar_publicaciones(request):
-    return render(request,'blog/listar_publicaciones.html',{})
+    publicaciones=Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request,'blog/listar_publicaciones.html',{'publicaciones':publicaciones})
+    #Lo que va dentro de las llaves: {'nombre de variable que va a recibir en la pagina web':nombre de lo que se va a enviar}
