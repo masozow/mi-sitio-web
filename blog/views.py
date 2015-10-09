af2e75fc8b4ee7fa5954fd806e8e05e9125556b1
masozow <marvingjo@hotmail.co>
2015-10-09 12:52:51 -0600

    Redireccion a post especifico

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postear
 # Create your views here.
@@ -6,3 +6,11 @@ def listar_publicaciones(request):
     publicaciones=Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request,'blog/listar_publicaciones.html',{'publicaciones':publicaciones})
     #Lo que va dentro de las llaves: {'nombre de variable que va a recibir en la pagina web':nombre de lo que se va a enviar}
+
+def detalle_publicaciones(request, pk):
+    #se iguala el parametro "pk" de la funcion, con la "pk" del modelo,
+    #la cual se genera automaticamente en todos los modelos
+    #publicacion=Postear.objects.filter(pk=pk)
+    #se usa get_object_or_404 para capturar los errores y poder redirigir a una pagina de error personalizada
+    publicacion=get_object_or_404(Postear,pk=pk)
+    return render (request,'blog/detalle_publicaciones.html',{'publicaciones':publicacion})
